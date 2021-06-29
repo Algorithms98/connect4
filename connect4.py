b1ea46f3250dd8a046329455f2997362d7441133
Josh Curry <joshuacurry2615@gmail.com>
2021-06-29 19:38:40 -0400

    Fixed indentation errors

--- connect4.py
+++ connect4.py
@@ -1,49 +1,45 @@
 class Connect4:
-    
-    
+
     def __init__(self, moveCounter, row, col, currentPlayer, isValid, pieces, isPlaying, boardArray):
-        
+
         self.moveCounter = 0
         self.row = 6
         self.col = 7
-        self.currentPlayer = 1 #initialized to player 1
+        self.currentPlayer = 1  # initialized to player 1
         self.isValid = true
         self.pieces = []
         self.isPlaying
         self.boardArray = [['' for i in range(col)] for j in range(row)]
-    
-    def printBoard(self):
-        
-        
-    def updateBoard(self):
-        
-        
-    def placeMove(self):  
-        
-        
-    def switchPlayers(self):  
-        
-        
-    def findEmptyRow(self):
-        
-        
-    def checkWinner(self):
-        
-        
-    def checkTie(self):
-        
-        
-    def verticalCheck(self):
-        
-        
-    def horizontalCheck(self):
-        
-        
-    def negativeDiagnonalCheck(self):
-        
-        
-    def positiveDiagonalCheck(self):
-        
-        
 
-    
+    def printBoard(self):
+        pass
+
+    def updateBoard(self):
+        pass
+
+    def placeMove(self):
+        pass
+
+    def switchPlayers(self):
+        pass
+
+    def findEmptyRow(self):
+        pass
+
+    def checkWinner(self):
+        pass
+
+    def checkTie(self):
+        pass
+
+    def verticalCheck(self):
+        pass
+
+    def horizontalCheck(self):
+        pass
+
+    def negativeDiagnonalCheck(self):
+        pass
+
+    def positiveDiagonalCheck(self):
+        pass
