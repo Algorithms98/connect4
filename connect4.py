cf8053e1a03da7f8c2fba77d290415c2ddb83ead
Prince Ndhlovu <princendhlovu@Princes-MacBook-Air.local>
2021-06-29 22:15:35 -0500

    Implemented most of the functionality except displaying to the screen

--- connect4.py
+++ connect4.py
@@ -9,7 +9,7 @@ class Connect4:
         self.isValid = True
         self.pieces = []
         self.isPlaying
-        self.boardArray = [['' for i in range(col)] for j in range(row)]
+        self.boardArray = [[' ' for i in range(col)] for j in range(row)]
 
     def printBoard(self):
         pass
@@ -17,29 +17,74 @@ class Connect4:
     def updateBoard(self):
         pass
 
-    def placeMove(self):
-        pass
+    def placeMove(currentPlayer, boardArray, row, column):
+    	boardArray[row][column] = currentPlayer
+        
+    def switchPlayers(currentPlayer):
 
-    def switchPlayers(self):
-        pass
+    	if currentPlayer == 'X':
+    		currentPlayer = 'O'
+    	else:
+    		currentPlayer == 'X'
 
-    def findEmptyRow(self):
-        pass
+    	return currentPlayer
+        
+
+    def findEmptyRow(boardArray, column):
+    	for i in range(row): 
+    		#rows = 6
+    		if boardArray[i][column] == ' ':
+    			return i 
 
     def checkWinner(self):
-        pass
+    	if verticalCheck(boardArray, currentPlayer) or horizontalCheck(boardArray, currentPlayer) or negativeDiagnonalCheck(boardArray, currentPlayer) or positiveDiagonalCheck(boardArray, currentPlayer):
+    		return True 
+    	return False
+
+
+    #finds the columns with openings 
+    def validColumns(boardArray):
+    	legal_moves = []
+
+    	for i in range(col):
+    		if boardArray[row-1][i] == ' ':
+    			legal_moves.append(i)
+    	return legal_moves
+        
 
     def checkTie(self):
-        pass
 
-    def verticalCheck(self):
-        pass
+    	#if there are no more legal moves and there is no winner -- its a tie 
+    	if len(validColumns(boardArray)) == 0 or checkWinner(boardArray, currentPlayer) == False:
+    		return True
+    	return False
 
-    def horizontalCheck(self):
-        pass
+    def verticalCheck(boardArray, currentPlayer):
+    	for i in range(col):
+    		for j in range(row-3):
+    			if boardArray[j][i] == currentPlayer and boardArray[j+1][i] == currentPlayer and boardArray[j+2][i] == currentPlayer and boardArray[j+3][i] == currentPlayer:
+    				return True
+    	return False
+
+    def horizontalCheck(boardArray, currentPlayer):
+    	for i in range(col-3):
+    		for j in range(row):
+    			if boardArray[j][i] == currentPlayer and boardArray[j][i+1] == currentPlayer and boardArray[j][i+2] == currentPlayer and boardArray[j][j+3] == currentPlayer:
+    				return True
+    	return False 
+        
 
     def negativeDiagnonalCheck(self):
-        pass
+        for i in range(col-3):
+        	for j in range(3, row):
+        		if boardArray[j][i] == currentPlayer and boardArray[j-1][i+1] == currentPlayer and boardArray[j-2][i+2] == currentPlayer and boardArray[j-3][j+3] == currentPlayer:
+        			return True
+        return False
 
     def positiveDiagonalCheck(self):
-        pass
+    	for i in range(col-3):
+    		for j in range(row-3):
+    			if boardArray[j][i] == currentPlayer and boardArray[j+1][i+1] == currentPlayer and boardArray[j+2][i+2] == currentPlayer and boardArray[j+3][j+3] == currentPlayer:
+        			return True
+        return False
+        
