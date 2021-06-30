9ecfcd2f0ba0962da26701bcdd1a83d1700777bf
Efosa Asemota <efosa.asemota@morehouse.edu>
2021-06-30 11:13:59 -0400

    implemented the main loop of the game

--- playGame.py
+++ playGame.py
@@ -4,6 +4,15 @@ from connect4 import *
 def main():
     connect4 = Connect4()
     connect4.printBoard()
+    
+    isPlaying = True
+    
+    while(isPlaying):
+        connect4.placeMove()
+        connect4.checkWinner()
+        
+        if(isPlaying == False):
+            break
 
 
 if __name__ == "__main__":
