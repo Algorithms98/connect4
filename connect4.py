c23e9e3dc4296d818d395322fe193e755e6606ce
Josh Curry <joshuacurry2615@gmail.com>
2021-06-30 11:16:38 -0400

    Filled in most functions and game loop

--- connect4.py
+++ connect4.py
@@ -5,15 +5,28 @@ class Connect4:
         self.moveCounter = 0
         self.row = 6
         self.col = 7
-        self.currentPlayer = 1  # initialized to player 1
+        self.currentPlayer = 'X'  # initialized to player 1
         self.isValid = True
         self.pieces = []
-        self.isPlaying = True
+        self.isOver = False
         self.boardArray = [
             ['-' for i in range(self.col)] for j in range(self.row)]
 
     def playGame(self):
         print("Welcome to Connect 4! Let's get started!")
+
+        while not self.isOver:
+            self.switchPlayers()
+            self.printBoard()
+            self.placeMove()
+            self.isOver = self.checkWinner() or self.checkTie()
+
+        if self.checkTie():
+            print("It's a tie!")
+            self.printBoard()
+        else:
+            print(f"Congrats, {self.currentPlayer}! You win!")
+            self.printBoard()
 
     def printBoard(self):
         print("\n")
@@ -21,31 +34,32 @@ class Connect4:
             print(" ".join(row))
         print("\n")
 
-    def updateBoard(self):
-        pass
+    def updateBoard(self, inputCol):
+        self.boardArray[self.findEmptyRow(
+            inputCol)][inputCol-1] = self.currentPlayer
 
     def placeMove(self):
-        userCol = input("Enter a column to place piece in: ")
-        while (not userCol.isnumeric()) or (userCol < 1 or userCol > 7):
+        userCol = int(input("Enter a column to place piece in: "))
+        while (userCol < 1 or userCol > 7) or self.findEmptyRow(userCol) == -1:
             print("Invalid input. Try again.")
-            userCol = input("Enter a column to place piece in: ")
-
-        self.boardArray[self.row][self.col] = self.currentPlayer
+            userCol = int(input("Enter a column to place piece in: "))
+        self.updateBoard(userCol)
 
     def switchPlayers(self):
 
         if self.currentPlayer == 'X':
             self.currentPlayer = 'O'
         else:
-            self.currentPlayer == 'X'
+            self.currentPlayer = 'X'
 
         return self.currentPlayer
 
-    def findEmptyRow(self):
+    def findEmptyRow(self, inputCol):
         for i in range(self.row):
             #rows = 6
-            if self.boardArray[i][self.col] == ' ':
+            if self.boardArray[i][inputCol-1] == '-':
                 return i
+        return -1
 
     def checkWinner(self):
         if self.verticalCheck() or self.horizontalCheck() or self.negativeDiagnonalCheck() or self.positiveDiagonalCheck():
@@ -58,14 +72,14 @@ class Connect4:
         legal_moves = []
 
         for i in range(self.col):
-            if self.boardArray[self.row-1][i] == ' ':
+            if self.boardArray[self.row-1][i] == '-':
                 legal_moves.append(i)
         return legal_moves
 
     def checkTie(self):
 
         # if there are no more legal moves and there is no winner -- its a tie
-        if len(validColumns(self.boardArray)) == 0 or checkWinner(self.boardArray, self.currentPlayer) == False:
+        if len(self.validColumns()) == 0 and self.checkWinner() == False:
             return True
         return False
 
--- playGame.py
+++ playGame.py
@@ -3,7 +3,7 @@ from connect4 import *
 
 def main():
     connect4 = Connect4()
-    connect4.printBoard()
+    connect4.playGame()
 
 
 if __name__ == "__main__":
